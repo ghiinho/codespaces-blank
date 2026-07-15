fc30c0c00795ea08d06e4a38e0564b43cfed4d56
ghiinho <67161687+ghiinho@users.noreply.github.com>
2026-07-15 08:57:46 +0000

    fix

--- src/sidebar.py
+++ src/sidebar.py
@@ -1,20 +1,25 @@
 import streamlit as st
 from src.utils.config_manager import carica_configurazione
 
-# Carica la configurazione per sapere quali voci mostrare
-config = carica_configurazione()
-
-# La Home e le Impostazioni sono sempre visibili
-pagine_disponibili = ["Home Page"]
-
-# Mostra i moduli solo se sono impostati su True in config.json
-if config["moduli"]["anagrafiche"]["attivo"]:
-    pagine_disponibili.append("Anagrafiche Iscritti")
-if config["moduli"]["presenze"]["attivo"]:
-    pagine_disponibili.append("Registro Presenze")
-if config["moduli"]["pagamenti"]["attivo"]:
-    pagine_disponibili.append("Gestione Pagamenti")
-if config["moduli"]["statistiche"]["attivo"]:
-    pagine_disponibili.append("Statistiche")
-
-pagine_disponibili.append("Impostazioni")
+def disegna_sidebar():
+    config = carica_configurazione()
+    
+    st.sidebar.title(config["general"].get("nome_campus", "Campus Estivo"))
+    
+    # Costruisci le pagine disponibili dinamicamente
+    pagine_disponibili = ["Home Page"]
+    
+    if config["moduli"]["anagrafiche"]["attivo"]:
+        pagine_disponibili.append("Anagrafiche Iscritti")
+    if config["moduli"]["presenze"]["attivo"]:
+        pagine_disponibili.append("Registro Presenze")
+    if config["moduli"]["pagamenti"]["attivo"]:
+        pagine_disponibili.append("Gestione Pagamenti")
+    if config["moduli"]["statistiche"]["attivo"]:
+        pagine_disponibili.append("Statistiche")
+        
+    pagine_disponibili.append("Impostazioni")
+    
+    # Selettore della pagina corrente
+    scelta = st.sidebar.radio("Navigazione", pagine_disponibili)
+    return scelta
