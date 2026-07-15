34fa38b003906ae848010a0cc1abd4a38420eb58
ghiinho <67161687+ghiinho@users.noreply.github.com>
2026-07-15 08:14:11 +0000

    spacchettamento funzioni

---
+++ src/views/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def mostra_home():
+    st.title("☀️ Gestionale Camp Estivo")
+    st.subheader("Benvenuto nel pannello di controllo!")
+    st.info("Seleziona una voce dal menu a sinistra per iniziare a lavorare.")
